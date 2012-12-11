73776f5edc6b47d5807e662f8c67d3034ee903a5
Adam Allen <adama@namm.org>
2012-12-11 12:47:57 -0800

    Initial Commit
    Signed-off-by: Adam Allen <adama@namm.org>

---
+++ sites/all/themes/offbeat/templates/block--bottom.tpl.php
@@ -0,0 +1,11 @@
+<div class="feat_box">
+
+  <?php print render($title_prefix); ?>
+  <?php if ($title): ?>
+    <h1<?php print $title_attributes; ?>><?php print $title; ?></h1>
+  <?php endif; ?>
+  <?php print render($title_suffix); ?>
+
+  <?php print $content; ?>
+
+</div><!-- /.block -->
---
+++ sites/all/themes/offbeat/templates/node--news-press--teaser.tpl.php
@@ -0,0 +1,11 @@
+<?php print render($title); ?>
+
+<?php print render($content['field_news_press_date']); ?>
+
+<?php print render($content['body']); ?>
+
+<?php print render($content['field_news_press_quote']); ?>
+
+<?php print render($content['field_news_press_link']); ?>
+
+<div class="hr640 clear"></div>
