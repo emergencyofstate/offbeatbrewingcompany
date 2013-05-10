0858d5324a2be9a44c7fa1fa4a057f119a43813b
Adam Allen <adama@namm.org>
2013-05-09 19:24:35 -0700

    Some template tweaks and removing a couple CSS files we're not using.
    Signed-off-by: Adam Allen <adama@namm.org>

--- sites/all/themes/offbeat/templates/node--news-press--teaser.tpl.php
+++ sites/all/themes/offbeat/templates/node--news-press--teaser.tpl.php
@@ -1,4 +1,6 @@
-<?php print render($title); ?>
+<?php print '<div class="press_title">'; ?>
+<?php print $title; ?>
+<?php print '</div>'; ?>
 
 <?php print render($content['field_news_press_date']); ?>
 
