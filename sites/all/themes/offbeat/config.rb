8e7101c0334e20bac462c1acc876b7e7974e6cb3
Adam Allen <adama@namm.org>
2013-05-31 11:51:44 -0700

    Some style and theme tweaks.
    Signed-off-by: Adam Allen <adama@namm.org>

--- sites/all/themes/offbeat/config.rb
+++ sites/all/themes/offbeat/config.rb
@@ -8,8 +8,8 @@
 
 
 # Change this to :production when ready to deploy the CSS to the live server.
-environment = :development
-#environment = :production
+#environment = :development
+environment = :production
 
 # In development, we can turn on the FireSass-compatible debug_info.
 firesass = false
@@ -18,8 +18,8 @@ firesass = false
 
 # Location of the theme's resources.
 css_dir         = "css"
-sass_dir        = "sass"
-extensions_dir  = "sass-extensions"
+#sass_dir        = "sass"
+#extensions_dir  = "sass-extensions"
 images_dir      = "images"
 javascripts_dir = "js"
 
@@ -41,7 +41,7 @@ javascripts_dir = "js"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
-output_style = (environment == :development) ? :expanded : :compressed
+output_style = (environment == :production) ? :expanded : :compressed
 
 # To enable relative paths to assets via compass helper functions. Since Drupal
 # themes can be installed in multiple locations, we don't need to worry about
