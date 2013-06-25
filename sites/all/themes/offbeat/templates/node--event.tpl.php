71991d5a03bc478e83c8a1045d7f3e0420c7ccdd
Adam Allen <adama@namm.org>
2013-06-24 21:11:24 -0700

    Adding classes
    Signed-off-by: Adam Allen <adama@namm.org>

--- sites/all/themes/offbeat/templates/node--event.tpl.php
+++ sites/all/themes/offbeat/templates/node--event.tpl.php
@@ -8,19 +8,17 @@
   $start_date_right = format_date($timestamp1, 'offbeat_date_frontpage');
   $end_date_right = format_date($timestamp2, 'offbeat_date_frontpage');
 
-  print '<div style="float: left;">';
+  print '<div class="event-date">';
   print $start_date_left . '&#8211;' . $end_date_left;
   print '</div>';
 
-  print '<div style="float: right; width: 500px">';
-  print $title . '<br />';
+  print '<div class="event-content">';
+  print '<div class="event-title">' . $title . '</div><br />';
   print $start_date_right . ', ' . $end_date_right;
-  print '<hr>';
-  //print '<div class="hr460"> </div>';
+  print '<div class="hr460"> </div>';
   //todo: field template for location
   print render($content['field_event_location']);
-  print '<hr>';
-  //print '<div class="hr460"> </div>';
+  print '<div class="hr460"> </div>';
   print $content['body'][0]['#markup'];
   print render($content['field_event_image']);
   print '</div>';
