7a33a2bca59e307089f3ed3378fa2e96385c9bb0
Adam Allen <adama@namm.org>
2013-07-16 15:15:53 -0700

    event teaser tweak
    Signed-off-by: Adam Allen <adama@namm.org>

--- sites/all/themes/offbeat/templates/node--event--teaser.tpl.php
+++ sites/all/themes/offbeat/templates/node--event--teaser.tpl.php
@@ -8,7 +8,7 @@
   $start_date_right = format_date($timestamp1, 'offbeat_date_full');
   $end_date_right = format_date($timestamp2, 'offbeat_date_full');
 
-  //print '<div class="event-container">';
+  print '<div class="event-container">';
   print '<div class="date-container"><div class="event-date">';
   if($content['field_event_date']['#items'][0]['value'] != $content['field_event_date']['#items'][0]['value2']) {
     print $start_date_left . '&ndash;' . $end_date_left;
@@ -27,4 +27,6 @@
 
   print render($content['field_event_location']);
   print '<div style="float: left;">' . $content['body']['#items'][0]['safe_value'] . '</div>';
+  print '</div>';
+  print '<hr />';
   print '</div>';
